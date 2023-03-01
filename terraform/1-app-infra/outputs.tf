e72e2866365c0e90e2fdb2068778e123b5747df5
anupam <anupamyadav0224@gmail.com>
2023-03-01 18:07:57 +0530

    output description updated.

--- terraform/1-app-infra/outputs.tf
+++ terraform/1-app-infra/outputs.tf
@@ -1,5 +1,5 @@
 // Resource outputs
 output "storage_bucket_selflink" {
   value       = google_storage_bucket.static_site.self_link
-  description = "The URL of the created cloud storage bucket"
+  description = "The self link of the created cloud storage bucket"
 }
