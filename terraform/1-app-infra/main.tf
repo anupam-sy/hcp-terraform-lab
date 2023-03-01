c88b699f3787b08600d3d9ea3f2348b67980b541
anupam <anupamyadav0224@gmail.com>
2023-03-01 18:22:57 +0530

    uniform access enabled for bucket.

--- terraform/1-app-infra/main.tf
+++ terraform/1-app-infra/main.tf
@@ -1,20 +1,17 @@
 locals {
   website_files = ["index.html", "404.html"]
-  storage_object_output_names = [
-    for object in google_storage_bucket_object.static_site_objects :
-    object.output_name
-  ]
 }
 
 resource "random_id" "suffix" {
   byte_length = 2
 }
 
-resource "google_storage_bucket" "static_site" {
-  project       = var.project_id
-  name          = "${var.storage_bucket_name}-${random_id.suffix.hex}"
-  location      = var.default_region
-  force_destroy = true
+resource "google_storage_bucket" "static_site_bkt" {
+  project                     = var.project_id
+  name                        = "${var.storage_bucket_name}-${random_id.suffix.hex}"
+  location                    = var.default_region
+  force_destroy               = true
+  uniform_bucket_level_access = true
 
   website {
     main_page_suffix = "index.html"
@@ -22,23 +19,16 @@ resource "google_storage_bucket" "static_site" {
   }
 }
 
-resource "google_storage_bucket_object" "static_site_objects" {
+resource "google_storage_bucket_object" "static_site_bkt_objects" {
   for_each = toset(local.website_files)
 
   name   = each.value
   source = "${path.module}/static-files/${each.value}"
-  bucket = google_storage_bucket.static_site.name
+  bucket = google_storage_bucket.static_site_bkt.name
 }
 
-resource "google_storage_object_access_control" "static_site_objects_acl" {
-  for_each = toset(local.storage_object_output_names)
-
-  object = each.value
-  bucket = google_storage_bucket.static_site.name
-  role   = "READER"
-  entity = "allUsers"
-
-  depends_on = [
-    google_storage_bucket_object.static_site_objects
-  ]
+resource "google_storage_bucket_iam_member" "static_site_bkt_iam" {
+  bucket = google_storage_bucket.static_site_bkt.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
 }
--- terraform/1-app-infra/outputs.tf
+++ terraform/1-app-infra/outputs.tf
@@ -1,5 +1,5 @@
 // Resource outputs
 output "storage_bucket_selflink" {
-  value       = google_storage_bucket.static_site.self_link
+  value       = google_storage_bucket.static_site_bkt.self_link
   description = "The self link of the created cloud storage bucket"
 }
